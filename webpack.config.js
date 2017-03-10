ab26837a56c7bc0b8d944465f526c4f9b03f1bb0
Vladimir Matveev <vmatveev@dataart.com>
2017-03-10 14:17:16 +0200

    new webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
 
     output: {
         path: path.resolve(__dirname, 'build'),
-        publicPath: '/assets/',
+        // publicPath: '/assets/',
         filename: 'bundle.js'
     },
 
